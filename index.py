71b3fb26b0485acab0985eb15622d9efe14246fc
David Pullinger <davpullinger@gmail.com>
2022-03-08 20:23:00 +0200

    Add implementation of all methods needed by protocol
    - added queries to database pertaining to each methods needs

--- index.py
+++ index.py
@@ -1,31 +1,21 @@
 from socket import *
-import sqlite3 as sl
-
-databaseCon = sl.connect("chatapp.db")
-
-
-def create_account(message):
-    return "create account"
+from protocol import *
+from hashlib import sha256
+import json
 
 
-def start_chat(message):
-    return "start chat"
-
-
-def send_message(message):
-    return "send message"
-
-
-def no_such_func(message):
-    return "ERROR: function " + message + " not found"
+def no_such_func(body):
+    return "ERROR: function not found"
 
 
 def use_protocol(message):
     return {
         "CREATE_ACC": create_account,
-        "START_CHAT": start_chat,
+        "LOGIN": login,
+        "UPDATE_MSGS": update_messages,
+        "CREATE_GROUP": create_group,
         "SEND_MSG": send_message,
-    }.get(message, no_such_func)(message)
+    }.get(message, no_such_func)
 
 
 def main():
@@ -34,11 +24,22 @@ def main():
     serverSocket.bind(("", serverPort))
     print("The server is ready to receive")
     while True:
-        # get message from user
+        # get message from any user
         message, clientAddress = serverSocket.recvfrom(2048)
         # use protocol to decide what to do
-        res = use_protocol(message.decode())
-        serverSocket.sendto(res.encode(), clientAddress)
+        message = message.decode()
+        # get header and body from user request
+        header = message[message.find("{//") + 3 : message.rfind("//}")]
+        ## find command and hash using header
+        command, hash = header.split(";")
+        ## find body
+        body = message[message.rfind("//}") + 3 :]
+        ## ensure message was not lost
+        if hash == sha256((command + body).encode()).hexdigest():
+            res = use_protocol(command)(json.loads(body))
+        else:
+            res = "MESSAGE INVALID"
+        serverSocket.sendto(json.dumps(res).encode(), clientAddress)
 
 
 if __name__ == "__main__":
